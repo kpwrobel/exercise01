42c891812a11bc24a4e209ebf0b8638704d26b19
wojcikewelina <gargula.ewe@gmail.com>
2020-01-20 18:51:22 +0100

    init html form

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var allPersons = [];
 
 window.onload = () => {
-
+    let name = document.getElementById('name-input')
+    let surnme = document.getElementById('name-input')
+    let name = document.getElementById('name-input')
+    let name = document.getElementById('name-input')
 }
 
 function generateList(tab) {
