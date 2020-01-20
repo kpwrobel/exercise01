6ce1dac3a1a57b0e32ab9f75fbc641ff21311411
wojcikewelina <gargula.ewe@gmail.com>
2020-01-20 19:46:37 +0100

    how to get values of all input

--- main.js
+++ main.js
@@ -2,12 +2,38 @@ const LOCALNAME = "tablePeopleStorage"
 var allPersons = [];
 
 window.onload = () => {
-const peopleTable = document.getElementById("peopleTable");
-const thead = document.getElementById("tableBody");
-    let name = document.getElementById('name-input')
-    let surnme = document.getElementById('name-input')
-    let name = document.getElementById('name-input')
-    let name = document.getElementById('name-input')
+    const peopleTable = document.getElementById("peopleTable");
+    const thead = document.getElementById("tableBody");
+
+
+    const nameFromInput = document.getElementById('nameInput');
+    const surnameFromInput = document.getElementById('surnameInput');
+    const genderFromInput = document.getElementsByTagName("select")[0];
+    const mobileFromInput = document.getElementById("mobileInput");
+    const emailFromInput = document.getElementById("emailInput");
+    let submitButton = document.getElementsByTagName("button")[0];
+
+
+    submitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        var person = {
+            name: nameFromInput.value,
+            surname: surnameFromInput.value,
+            gender: genderFromInput.value,
+            mobile: mobileFromInput.value,
+            email: emailFromInput
+        }
+        console.log(person);
+    })
+
+
+    var form = document.querySelector("form")
+    form.onSubmit = (elem) => {
+        debugger
+        elem.preventDefault();
+    }
+
 }
 
 function generateList(tab) {
